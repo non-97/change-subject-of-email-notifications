6842cf504453c0c6bcc5ca9d7a629fdde5df50c3
non-97 <nonmonaka07@gmail.com>
2022-02-18 18:50:57 +0900

    Initial Commit

--- lib/change-subject-of-email-notifications-stack.ts
+++ lib/change-subject-of-email-notifications-stack.ts
@@ -1,16 +1,66 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  Fn,
+  Stack,
+  StackProps,
+  aws_logs as logs,
+  aws_sns as sns,
+  aws_sns_subscriptions as sns_subscriptions,
+  aws_iam as iam,
+  aws_stepfunctions as sfn,
+  aws_stepfunctions_tasks as tasks,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+interface ChangeSubjectOfEmailNotificationsStackProps extends StackProps {
+  emailAddress: string;
+}
 
 export class ChangeSubjectOfEmailNotificationsStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: ChangeSubjectOfEmailNotificationsStackProps
+  ) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // SNS Email
+    const emailSnsTopic = new sns.Topic(this, "EmailSnsTopic");
+    emailSnsTopic.addSubscription(
+      new sns_subscriptions.EmailSubscription(props.emailAddress)
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ChangeSubjectOfEmailNotificationsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const stackUniqueId = Fn.select(2, Fn.split("/", this.stackId));
+
+    // CloudWatch Logs for State Machines
+    const changeSubjectEmailNotificationStateMachineLogGroup =
+      new logs.LogGroup(
+        this,
+        "ChangeSubjectEmailNotificationStateMachineLogGroup",
+        {
+          logGroupName: `/aws/vendedlogs/states/changeSubjectEmailNotificationStateMachineLogGroup-${stackUniqueId}`,
+          retention: logs.RetentionDays.TWO_WEEKS,
+        }
+      );
+
+    // Change subject of email notification State Machine
+    const publishSns = new tasks.CallAwsService(this, "PublishSns", {
+      service: "sns",
+      action: "publish",
+      parameters: {
+        TopicArn: emailSnsTopic.topicArn,
+        "Message.$": "$.message",
+        "Subject.$": "$.subject",
+      },
+      iamResources: [emailSnsTopic.topicArn],
+    });
+
+    new sfn.StateMachine(this, "ChangeSubjectEmailNotificationStateMachine", {
+      definition: publishSns,
+      logs: {
+        destination: changeSubjectEmailNotificationStateMachineLogGroup,
+        level: sfn.LogLevel.ALL,
+      },
+      tracingEnabled: true,
+    });
   }
 }
